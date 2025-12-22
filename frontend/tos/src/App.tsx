5628b8315624a9089088cd3c2c8b3edd5e5038e1
Sahil Chopra <choprasahil.sc@gmail.com>
2025-12-22 05:11:39 -0800

    Setup Frontend

---
+++ frontend/tos/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className="flex min-h-screen items-center justify-center bg-black text-white">
+			<h1 className="text-5xl font-bold tracking-tight">Town Of Salem</h1>
+		</div>
+	);
+}
+
+export default App;
