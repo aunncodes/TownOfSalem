9165807fa0d8e0cd8b4b0984e01dae2a8720e23d
Sahil Chopra <choprasahil.sc@gmail.com>
2026-05-24 15:41:32 -0700

    Update sheriff.py

--- backend/engine/tos/abilities/sheriff.py
+++ backend/engine/tos/abilities/sheriff.py
@@ -11,7 +11,7 @@ class SheriffAbility(TargetedNightAbility):
 
     def apply(self, state, intent, ctx):
         target = state.require_player(intent.target)
-        suspicious = (Tag.DETECTION_IMMUNE not in target.role.tags) or (Status.FRAMED in target.status) # Detection immune term is a bit vague, but simply all roles that should appear innocent are "Detection immune"
+        suspicious = (Tag.DETECTION_IMMUNE not in target.role.tags) or (Status.FRAMED in target.status) # Detection immune term is a bit vague, but simply all roles that should appear innocent are "Detection immune". Simply implemented like this to match the real game.
 
         ctx.add_event(GameEvent(
             event_type=GameEventType.SHERIFF_RESULT,
